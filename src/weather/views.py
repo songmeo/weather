a7c23ecab0bf15543aa0390c7eb3ae68f594a591
sopham <shuong1012@gmail.com>
2020-09-17 16:50:14 +0300

    rm unnecessary import

--- src/weather/views.py
+++ src/weather/views.py
@@ -1,12 +1,6 @@
 from .models import Location, Parameter
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from rest_framework import viewsets
-from rest_framework import generics
 from .serializers import LocationSerializer, ParameterSerializer
-from rest_framework.decorators import action
-from rest_framework import status
-from rest_framework.views import APIView 
 from .helper import get_parameter_values
 
 class LocationViewSet(viewsets.ModelViewSet):
